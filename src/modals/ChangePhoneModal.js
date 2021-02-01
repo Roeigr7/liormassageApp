aeff46ec5889d33f8f9d588b402fc565643710a3
Roei Grinshpan <roeigr7@gmail.com>
2021-02-01 06:46:56 +0200

    ready

--- src/modals/ChangePhoneModal.js
+++ src/modals/ChangePhoneModal.js
@@ -1,93 +1,95 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Context as AuthContext } from "../context/auth-context";
-import { Context as ModalContext } from "../context/modal-context";
-import * as Animatable from "react-native-animatable";
-
-import { TextInput } from "react-native-gesture-handler";
-import AnimatedButton from "../customStyles/AnimatedButton";
-import { phoneValidator } from "../helpers/ClientValidators";
-import { phoneError } from "../../utilities/texts/profile";
+import React, { useContext, useRef, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Context as AuthContext } from '../context/auth-context';
+import { Context as ModalContext } from '../context/modal-context';
+import * as Animatable from 'react-native-animatable';
+import AnimatedButton from '../customStyles/AnimatedButton';
+import { phoneValidator } from '../helpers/ClientValidators';
+import { phoneError } from '../../utilities/texts/profile';
 const ChangePhoneModal = () => {
-	const animationRef = useRef(null);
-	const [phone, setPhone] = useState("");
-	const { state, updateUser } = useContext(AuthContext);
-	const { hideModal } = useContext(ModalContext);
-	const [error, setError] = useState(false);
+  const animationRef = useRef(null);
+  const [phone, setPhone] = useState('');
+  const { state, updateUser } = useContext(AuthContext);
+  const { hideModal } = useContext(ModalContext);
+  const [error, setError] = useState(false);
 
-	const phoneHandler = async () => {
-		if (animationRef && error) {
-			animationRef.current?.swing();
-			setError(false);
-		}
-		if (phoneValidator(phone)) {
-			await updateUser(state.userId, state.token, "phone", phone).then(() => {
-				hideModal();
-			});
-		} else setError(phoneError);
-	};
+  const phoneHandler = async () => {
+    if (animationRef && error) {
+      animationRef.current?.swing();
+      setError(false);
+    }
+    if (phoneValidator(phone)) {
+      await updateUser(state.userId, state.token, 'phone', phone).then(() => {
+        hideModal();
+      });
+    } else setError(phoneError);
+  };
 
-	return (
-		<View style={styles.container}>
-			<>
-				<Text style={styles.text}>בחר מספר טלפון חדש</Text>
-				<TextInput
-					keyboardType="numeric"
-					placeholder="הקש מספר טלפון"
-					style={styles.textinput}
-					onChangeText={setPhone}
-				/>
-			</>
+  return (
+    <View style={styles.container}>
+      <>
+        <Text style={styles.text}>בחר מספר טלפון חדש</Text>
+        <TextInput
+          keyboardType='numeric'
+          placeholder='הקש מספר טלפון'
+          style={styles.textinput}
+          onChangeText={setPhone}
+        />
+      </>
 
-			{error && (
-				<Animatable.Text
-					ref={animationRef}
-					animation={"swing"}
-					easing={"ease-out"}
-					duration={700}
-					style={[styles.errortext, { color: "#d9b310" }]}
-				>
-					{error}
-				</Animatable.Text>
-			)}
-			<AnimatedButton onPress={phoneHandler} buttonStyle={styles.animatedbtn} text="אישור" />
-		</View>
-	);
+      {error && (
+        <Animatable.Text
+          ref={animationRef}
+          animation={'swing'}
+          easing={'ease-out'}
+          duration={700}
+          style={[styles.errortext, { color: '#d9b310' }]}
+        >
+          {error}
+        </Animatable.Text>
+      )}
+      <AnimatedButton
+        onPress={phoneHandler}
+        buttonStyle={styles.animatedbtn}
+        text='אישור'
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		alignItems: "center",
-	},
-	text: {
-		color: "white",
-		fontFamily: "heeboBold",
-		fontSize: 18,
-	},
-	textinput: {
-		borderRadius: 5,
-		padding: 8,
-		minWidth: "60%",
-		marginVertical: 10,
-		textAlign: "center",
-		backgroundColor: "#f0f0f0",
-	},
-	animatedbtn: {
-		backgroundColor: "#d9b310",
-		color: "white",
-		minWidth: "40%",
-		marginVertical: 5,
-		paddingVertical: 8,
-		borderRadius: 6,
-		marginBottom: 20,
-	},
-	errortext: {
-		textAlign: "center",
-		marginBottom: 10,
-		fontSize: 16,
-		color: "white",
-		fontFamily: "heebo",
-	},
+  container: {
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontFamily: 'heeboBold',
+    fontSize: 18,
+  },
+  textinput: {
+    borderRadius: 5,
+    padding: 8,
+    minWidth: '60%',
+    marginVertical: 10,
+    textAlign: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  animatedbtn: {
+    backgroundColor: '#d9b310',
+    color: 'white',
+    minWidth: '40%',
+    marginVertical: 5,
+    paddingVertical: 8,
+    borderRadius: 6,
+    marginBottom: 20,
+  },
+  errortext: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 16,
+    color: 'white',
+    fontFamily: 'heebo',
+  },
 });
 
 export default ChangePhoneModal;
--- src/modals/PhoneModal.js
+++ src/modals/PhoneModal.js
@@ -1,100 +1,110 @@
-import React, { useContext, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Context as AuthContext } from "../context/auth-context";
-import Modal from "react-native-modal";
-import { TextInput } from "react-native-gesture-handler";
-import AnimatedButton from "../customStyles/AnimatedButton";
-import { phoneValidator } from "../helpers/ClientValidators";
-import * as RootNavigator from "../navigators/RootNavigator";
+import React, { useContext, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Context as AuthContext } from '../context/auth-context';
+import Modal from 'react-native-modal';
+
+import AnimatedButton from '../customStyles/AnimatedButton';
+import { phoneValidator } from '../helpers/ClientValidators';
+import * as RootNavigator from '../navigators/RootNavigator';
 const PhoneModal = () => {
-	const [visible, setVisible] = useState(true);
-	const [phone, setPhone] = useState("");
-	const { state, updateUser } = useContext(AuthContext);
-	const [error, setError] = useState(false);
-	const phoneHandler = () => {
-		phoneValidator(phone) ? updateUser(state.userId, state.token, "phone", phone) : setError(true);
-	};
-	const closeModal = () => {
-		setVisible(false);
-		RootNavigator.navigate("Main");
-	};
-	return (
-		<View style={styles.containerbg}>
-			<Modal
-				animationInTiming={500}
-				backdropOpacity={0.5}
-				onBackdropPress={closeModal}
-				isVisible={visible}
-				animationIn="bounceIn"
-				animationOut="fadeOutDown"
-			>
-				<View style={styles.container}>
-					<Text style={styles.text}>לא קיים מספר טלפון במערכת</Text>
-					<Text style={styles.text}>יש להוסיף מספר טלפון כדי לקבוע תור</Text>
-					<TextInput
-						keyboardType="numeric"
-						placeholder="הקש מספר טלפון"
-						style={[styles.textinput]}
-						onChangeText={setPhone}
-					/>
-					{error && <Text style={styles.errortext}>נא להקיש מספר טלפון בעל 10 ספרות {"\n"} שמתחיל בספרה 0</Text>}
-					<AnimatedButton onPress={phoneHandler} buttonStyle={styles.animatedbtn} text="אישור" />
-				</View>
-			</Modal>
-		</View>
-	);
+  const [visible, setVisible] = useState(true);
+  const [phone, setPhone] = useState('');
+  const { state, updateUser } = useContext(AuthContext);
+  const [error, setError] = useState(false);
+  const phoneHandler = () => {
+    phoneValidator(phone)
+      ? updateUser(state.userId, state.token, 'phone', phone)
+      : setError(true);
+  };
+  const closeModal = () => {
+    setVisible(false);
+    RootNavigator.navigate('Main');
+  };
+  return (
+    <View style={styles.containerbg}>
+      <Modal
+        animationInTiming={500}
+        backdropOpacity={0.5}
+        onBackdropPress={closeModal}
+        isVisible={visible}
+        animationIn='bounceIn'
+        animationOut='fadeOutDown'
+      >
+        <View style={styles.container}>
+          <Text style={styles.text}>לא קיים מספר טלפון במערכת</Text>
+          <Text style={styles.text}>יש להוסיף מספר טלפון כדי לקבוע תור</Text>
+          <TextInput
+            keyboardType='numeric'
+            placeholder='הקש מספר טלפון'
+            style={[styles.textinput]}
+            onChangeText={setPhone}
+          />
+          {error && (
+            <Text style={styles.errortext}>
+              נא להקיש מספר טלפון בעל 10 ספרות {'\n'} שמתחיל בספרה 0
+            </Text>
+          )}
+          <AnimatedButton
+            onPress={phoneHandler}
+            buttonStyle={styles.animatedbtn}
+            text='אישור'
+          />
+        </View>
+      </Modal>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	containerbg: {
-		flex: 1,
-		backgroundColor: "#328cc1",
-	},
-	container: {
-		borderColor: "white",
-		borderRadius: 20,
-		borderWidth: 1,
-		borderStyle: "dotted",
-		paddingVertical: 10,
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "#1d2731",
-	},
-	text: {
-		marginVertical: 5,
-		color: "white",
-		fontFamily: "heeboBold",
-		fontSize: 16,
-	},
-	errortext: {
-		textAlign: "center",
-		marginTop: 8,
-		color: "white",
-		fontFamily: "heebo",
-		fontSize: 14,
-	},
-	textinput: {
-		backgroundColor: "#FFCF40",
-		width: "80%",
-		height: 40,
-		borderRadius: 5,
-		padding: 10,
-		margin: 5,
-		marginTop: 10,
-		marginVertical: 0,
+  containerbg: {
+    flex: 1,
+    backgroundColor: '#328cc1',
+  },
+  container: {
+    borderColor: 'white',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1d2731',
+  },
+  text: {
+    marginVertical: 5,
+    color: 'white',
+    fontFamily: 'heeboBold',
+    fontSize: 16,
+  },
+  errortext: {
+    textAlign: 'center',
+    marginTop: 8,
+    color: 'white',
+    fontFamily: 'heebo',
+    fontSize: 14,
+  },
+  textinput: {
+    backgroundColor: '#FFCF40',
+    width: '80%',
+    height: 40,
+    borderRadius: 5,
+    padding: 10,
+    margin: 5,
+    marginTop: 10,
+    marginVertical: 0,
 
-		textAlign: "center",
-		width: "60%",
-		backgroundColor: "#f0f0f0",
-	},
-	animatedbtn: {
-		backgroundColor: "#d9b310",
-		color: "white",
-		minWidth: "40%",
-		marginVertical: 15,
-		paddingVertical: 8,
-		borderRadius: 6,
-	},
+    textAlign: 'center',
+    width: '60%',
+    backgroundColor: '#f0f0f0',
+  },
+  animatedbtn: {
+    backgroundColor: '#d9b310',
+    color: 'white',
+    minWidth: '40%',
+    marginVertical: 15,
+    paddingVertical: 8,
+    borderRadius: 6,
+  },
 });
 
 export default PhoneModal;
